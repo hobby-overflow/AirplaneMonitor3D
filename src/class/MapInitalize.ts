164052ae84e8851c2e8f4eedfe751289f7e3575f
hobby-overflow <radio_old_boy_cq@yahoo.co.jp>
2022-12-21 13:38:37 +0900

    fix read config.json.

--- src/class/MapInitalize.ts
+++ src/class/MapInitalize.ts
@@ -4,7 +4,18 @@ import TOKEN from "./Token";
 const satellite = 'mapbox://styles/mapbox/satellite-v9'
 const satellite_streets = 'mapbox://styles/mapbox/satellite-streets-v11'
 
-export const MapInitialize= () => {
+let Config: Config;
+
+export const MapInitialize = () => {
+
+  window.api.send('read_config', null);
+  window.api.on('read_config', (arg: string) => {
+    if (arg != null) {
+      Config = JSON.parse(arg) as Config;
+    }
+  })
+
+  // TOKEN is not use?
   const mapCenter = new mapboxgl.LngLat(141.6769, 42.7831);
   const map = new mapboxgl.Map({
     container: document.getElementById('mapContainer') as HTMLElement,
@@ -13,7 +24,7 @@ export const MapInitialize= () => {
     zoom: 16,
     antialias: true,
     style: satellite,
-    accessToken: TOKEN,
+    accessToken: Config.map.api_id,
   });
   return map
 }
