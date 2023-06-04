84e1a256202d9859b5c0f7d4cbb71eee145d46d7
hobby-overflow <radio_old_boy_cq@yahoo.co.jp>
2023-06-04 10:46:12 +0900

    Change import, add functions Scenery.ts

--- src/class/Scenery.ts
+++ src/class/Scenery.ts
@@ -1,37 +1,88 @@
-import * as THREE from 'three';
 import { LngLat } from 'mapbox-gl';
-import { Vector3 } from 'three';
+import {
+  AmbientLight,
+  Mesh,
+  MeshStandardMaterial,
+  Scene,
+  Vector3,
+} from 'three';
 import { GeoMath } from './GeoMath';
-import { ColladaLoader } from '../lib/ColladaLoader.js';
+
+import {
+  Collada,
+  ColladaLoader,
+} from 'three/examples/jsm/loaders/ColladaLoader';
+import { Object3D } from 'three';
+import { Aircraft } from './Aircraft';
+import { BoxGeometry } from 'three';
 
 export class Scenery {
   public getScene = () => {
     return this.scene;
   };
 
-  private scene: THREE.Scene;
+  private scene: Scene;
+  private origin: LngLat;
 
   constructor(origin: LngLat) {
-    this.scene = new THREE.Scene();
+    this.scene = new Scene();
     this.initScene(origin);
+    this.origin = origin;
   }
 
   private initScene = (origin: LngLat) => {
-    const sunLight = new THREE.DirectionalLight(0xffffff);
-    sunLight.position.set(0, 70, 100).normalize();
-    this.scene.add(sunLight);
+    let b737Model: Object3D = new Object3D();
 
-    const boxGeometry = new THREE.BoxGeometry(20, 20, 20);
-    const boxMaterial = new THREE.MeshNormalMaterial();
-    const box = new THREE.Mesh(boxGeometry, boxMaterial);
-
-    const rwy01L = new LngLat(141.6927, 42.7622);
-    const box1 = box.clone();
-    const pos1 = GeoMath.getLocation(origin, rwy01L);
-    box1.position.set(pos1.x, pos1.y, pos1.z);
-    box1.rotation.y = 352;
-    this.scene.add(box1);
+    const loader = new ColladaLoader();
+    loader.load('./B737-800.dae', (data: Collada) => {
+      const model = data.scene;
+      const modelScale = 1;
+      model.scale.set(modelScale, modelScale, modelScale);
+      b737Model.copy(model);
+      const airplane = new Object3D().copy(model);
+      const rwy01L = new LngLat(141.6927, 42.7622);
+      const pos1 = GeoMath.getLocation(origin, rwy01L);
+      airplane.position.set(pos1.x, pos1.y, pos1.z);
+      airplane.rotation.y = 352;
+      this.scene.add(airplane);
+    });
+    // const sunLight = new THREE.DirectionalLight(0xffffff);
+    // sunLight.position.set(0, 70, 100).normalize();
+    const ambientLight = new AmbientLight(0xffffff, 1.0);
+    this.scene.add(ambientLight);
   };
 
-  public addObject = (position: LngLat | Vector3) => {};
+  public setLocation = (aircraft: Aircraft) => {
+    const scale = 400;
+    // const boxGeometry = new BoxGeometry(scale, (scale * 3), scale)
+    const boxGeometry = new BoxGeometry(1, 1, 1);
+    const boxMaterial = new MeshStandardMaterial({ color: 0xff4500 });
+
+    console.log(aircraft.info);
+    const lngLat = new LngLat(aircraft.info.Long, aircraft.info.Lat);
+    const location = GeoMath.getLocation(this.origin, lngLat);
+    console.log(`${aircraft.info.Reg}: ${location}`);
+
+    let obj = this.scene.getObjectByName(aircraft.info.Icao);
+
+    if (obj == null) {
+      const acObj = new Mesh(boxGeometry, boxMaterial);
+      acObj.scale.set(scale, aircraft.info.Alt / 3.281, scale);
+      acObj.name = aircraft.info.Icao;
+      acObj.position.copy(location);
+      const whd = new Vector3().copy(acObj.scale);
+      acObj.position.setY(whd.y / 2);
+      this.scene.add(acObj);
+    } else if (obj != null) {
+      obj.position.copy(location);
+      obj.position.setY(obj.scale.y / 2)
+    }
+  };
+
+  public removeAircraft = (aircraft: Aircraft) => {
+    let obj = this.scene.getObjectByName(aircraft.info.Icao);
+    if (obj != null) {
+      this.scene.remove(obj);
+    }
+  };
 }
